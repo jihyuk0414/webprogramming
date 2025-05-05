700c80ffa4d3ec3df5c32c2510c392a11d5982ac
jihyuk0414 <dealon2580@naver.com>
2025-05-05 18:35:14 +0900

    DB 설정 및 기본 로직 구현

---
+++ app.js
@@ -0,0 +1,510 @@
+const express = require("express");
+const router = express.Router();
+const app = express();
+const port = 3001;
+const db = require('./db');
+var fs = require('fs');
+var ejs = require('ejs');
+
+const bcrypt = require('bcrypt');
+
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+
+const mysql = require('mysql2');
+
+const session = require('express-session');
+const bodyParser = require('body-parser');
+
+const livereload = require('livereload');
+const livereloadMiddleware = require('connect-livereload');
+
+const liveServer = livereload.createServer({
+  exts: ['html', 'css', 'ejs', 'js'],
+  debug: true
+});
+
+liveServer.watch(__dirname);
+
+app.use(livereloadMiddleware());
+
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
+
+const connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'1234',
+  port:3306,
+  database:'nodejs'
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.get('/', function (req, res, next) {
+  db.getAllMemos((rows) => {
+    db.getRankMemos((data) =>{
+      if(req.session.user){
+        res.render(__dirname + '/views/main.ejs', { rows: rows, 
+                                                    data: data,
+                                                    user: req.session.user });
+      }
+      else{
+        res.render(__dirname + '/views/main.ejs', { rows: rows, data: data, });
+      }
+    })
+
+    
+  });
+});
+
+app.listen(port, () => {
+  console.log("서버가 실행됩니다.");
+  console.log("http://localhost:3001/");
+});
+
+app.use('/', express.static("css"));
+app.use('/', express.static("images"));
+app.use('/', express.static("js"));
+
+app.get("/board/:cur", function (req, res) {
+
+  var searchKeyword = req.query.search || "";
+
+  //페이지당 게시물 수 : 한 페이지 당 10개 게시물
+  var page_size = 10;
+  //페이지의 갯수 : 1 ~ 10개 페이지
+  var page_list_size = 10;
+  //limit 변수
+  var no = "";
+  //전체 게시물의 숫자
+  var totalPageCount = 0;
+  if (!searchKeyword) {
+    var queryString = 'SELECT count(*) AS cnt FROM board';
+  } else {
+    var queryString = `SELECT count(*) AS cnt FROM board WHERE title LIKE ${connection.escape('%' + searchKeyword + '%')}`;
+  }
+  
+  connection.query(queryString, function (error2, data) {
+    if (error2) {
+      console.log(error2 + "메인 화면 mysql 조회 실패");
+      return
+    }
+
+    totalPageCount = data[0].cnt
+
+    var curPage = req.params.cur || 1;
+
+    if (totalPageCount < 0) {
+      totalPageCount = 0
+    }
+
+    var totalPage = Math.ceil(totalPageCount / page_size);
+    var totalSet = Math.ceil(totalPage / page_list_size);       
+    var curSet = Math.ceil(curPage / page_list_size) 
+    var startPage = ((curSet - 1) * 10) + 1 
+    var endPage = (startPage + page_list_size) - 1;
+
+
+    if (curPage < 0) {
+      no = 0
+    } else {
+      no = (curPage - 1) * 10
+    }
+     var result2 = {
+      "curPage": curPage,
+      "page_list_size": page_list_size,
+      "page_size": page_size,
+      "totalPage": totalPage,
+      "totalSet": totalSet,
+      "curSet": curSet,
+      "startPage": startPage,
+      "endPage": endPage,
+      "searchKeyword": searchKeyword,
+    };
+
+    db.getMemosPagenation(no, page_size, searchKeyword, (rows) => {
+      if(req.session.user){
+        res.render(__dirname + '/views/board.ejs', {data: rows,
+                                                    pasing: result2,
+                                                    user: req.session.user});
+      }
+      else{
+        res.render(__dirname + '/views/board.ejs', {data: rows,
+          pasing: result2});
+      }
+    });
+  });
+});
+
+app.get("/main", function (req, res) {
+
+  res.redirect('board/1');
+
+});
+
+app.get('/write', (req, res) => {
+
+  let today = new Date();
+  today.setHours(today.getHours() + 9);
+  let date = today.toISOString().replace('T', ' ').substring(0, 10);
+
+  if(req.session.user){
+    res.render(__dirname + '/views/write.ejs', { date, user: req.session.user });
+  }
+  else{
+    res.status(400).json({ message: '로그인 후 이용가능합니다.' });
+  }
+  
+});
+
+app.get('/checkSession', (req, res) => {
+  if (req.session.user) {
+    res.json({ sessionExists: true });
+  } else {
+    res.json({ sessionExists: false });
+  }
+});
+
+
+app.use('/insert', function (req, res, next) {
+  let title = req.body.title;
+  let name = req.body.name;
+  let content = req.body.content;
+  if(title.length <= 0){
+    res.status(400).json({ message: '글 제목을 입력해주세요.' });
+        return;
+  }
+  else if(content.length <= 0){
+    res.status(400).json({ message: '내용을 입력해주세요.' });
+    return;
+  }
+  db.insertMemo(content, title, name);
+
+  res.redirect('/main');
+});
+
+app.use('/modify', function (req, res, next) {
+  let title = req.query.title;
+  let value = req.query.value;
+  let name = req.query.name;
+  
+  db.getInfo(title, value, name, (rows) => {
+    res.render(__dirname + '/views/modify.ejs', { rows: rows, user: req.session.user });
+  });
+});
+
+app.use('/update', function (req, res, next) {
+  let title = req.query.title;
+  let name = req.query.name;
+  let content = req.body.content;
+  let value = req.query.value;
+
+  db.memoUpdate(title, value, name, content);
+  
+  res.redirect(`/memo/?title=${title}&value=${value}&name=${name}`);
+});
+
+app.use('/delete', function (req, res, next) {
+  let title = req.body.title;
+  let name = req.body.name;
+  let value = req.body.value;
+
+  db.memoDelete(title, name, value);
+
+  res.send('게시글이 삭제되었습니다.');
+});
+
+
+app.get('/memo', (req, res) => {
+  let title = req.query.title;
+  let value = req.query.value;
+  db.watchCount(title, value);
+  db.getMemo(title, value, (rows) => {
+
+    if(req.session.user){
+      res.render(__dirname + '/views/memo.ejs', { rows: rows, user: req.session.user });
+    }
+    else{
+      res.render(__dirname + '/views/memo.ejs', { rows: rows });
+    }
+  });
+});
+
+app.get('/checkMemoSession', (req, res) => {
+  
+  let name = req.query.name;
+
+  if(req.session.user){
+
+    if (req.session.user.name === name) {
+      res.json({ sessionCompare: true });
+    } else {
+      res.json({ sessionCompare: false });
+    }
+
+  }else{
+    res.json({ sessionCompare: false });
+  }
+});
+
+app.get('/loginpage', (req, res) => {
+  res.render(__dirname + '/views/login.html');
+});
+
+app.use('/login', (req, res) => {
+  let id = req.body.id;
+  let password = req.body.password;
+  let previousUrl = req.body.prevUrl;
+  let main = '/';
+  console.log(previousUrl);
+
+  db.checkUser(id, (rows) => {
+    if (rows.length > 0) {
+      let same = bcrypt.compareSync(password, rows[0].password);
+      console.log("DB에서 가져온 사용자 정보:", rows[0]);
+
+      if (same) {
+        req.session.user = {
+          id: id,
+          name: id,
+          nickname: rows[0].nickname
+        };
+        if (previousUrl) {
+          res.send(previousUrl);
+        } else {
+          res.redirect(main);
+        }
+      } else {
+        res.status(400).json({ message: '비밀번호를 확인해주세요.' });
+        return;
+      }
+    } else {
+      res.status(400).json({ message: '아이디를 확인해주세요.' });
+      return;
+    }
+  });
+});
+
+
+app.get('/logout', (req, res) => {
+  
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect('/');
+  });
+});
+
+
+app.get('/signin', (req, res) => {
+  res.render(__dirname + '/views/signin.html');
+});
+
+app.use('/sign', (req, res) => {
+
+  let id = req.body.id;
+  let password = req.body.password;
+  let nickname = req.body.nickname;
+  console.log(nickname);
+
+  const encryptedPassowrd = bcrypt.hashSync(password, 10);
+
+  if (id.length < 10) {
+    res.status(400).json({ message: '아이디는 최소 10글자 이상이어야 합니다.' });
+    return;
+  }
+  else if (password.length < 5) {
+    res.status(400).json({ message: '비밀번호는 최소 5글자 이상이어야 합니다.' });
+    return;
+  }
+  else if (nickname.length < 3) {
+    res.status(400).json({ message: '닉네임은 최소 3글자 이상이어야 합니다.' });
+    return;
+  }
+  else {
+    db.getUser(id, nickname, (rows) => {
+      if (rows.length <= 0) {
+        console.log(rows);
+        db.insertUser(id, encryptedPassowrd, nickname);
+
+        res.redirect('/loginpage');
+      }
+      else {
+        res.status(400).json({ message: '이미 존재하는 아이디 입니다.' });
+        return;
+      }
+    });
+
+  }
+});  
+
+app.use('/commend', (req, res) => {
+  let currentDate = new Date();
+  const dateString = currentDate.toISOString();
+  const formattedDate = dateString.replace(/[-T:.Z]/g, '');
+  
+  let value = req.query.value;
+  let content = req.query.textBox;
+  let user = req.session.user.name;
+  let commendId = value + "_" + formattedDate;
+  if(content <= 0){
+    res.status(400).json({ message: '댓글을 작성해주세요.' });
+    return;
+  }
+  else{
+    db.commendInsert(value, content, user, commendId);
+    res.send('댓글 작성이 완료되었습니다.');
+  }
+  
+});
+
+
+app.use('/getCommend', (req, res) => {
+  let value = req.query.value;
+  let offset = parseInt(req.query.offset) || 0; 
+  let limit = 5; 
+
+  db.getCommend(value, offset, limit, (rows) => {
+    res.json(rows);
+  });
+});
+
+
+app.get('/getCommentCount', (req, res) => {
+  let value = req.query.value;  
+  db.getCommendCount(value, (rows) => {
+    res.json(rows); 
+  });
+});
+
+app.use('/getRecommend', (req, res) => {
+  let commendId = req.query.id;
+
+  let offset = parseInt(req.query.offset);
+  let limit = 5;
+
+  db.getRecommend(commendId, offset, limit, (rows) => {
+    res.json(rows);
+    console.log(rows);
+  });
+});
+
+app.use('/writeRecommend', (req, res) => {
+  let recommend_content = req.query.recommendtext; 
+  let commend_id = req.query.id; 
+  let recommend_id = commend_id + "_" + Math.floor(Math.random() * 9999); 
+
+
+  
+  if (req.session.user) {
+
+    let recommend_name = req.session.user.name;
+
+    if(recommend_content.length <= 0){
+      res.status(400).json({ message: '답글을 작성해주세요.'});
+      return;
+    }
+    else{
+
+      db.insertRecommend(commend_id, recommend_name, recommend_content, recommend_id);
+      res.send('답글 작성이 완료되었습니다.');
+    }
+
+  } else {
+    res.send('로그인 후 사용가능합니다.');
+  }
+
+});
+
+// 좋아요 기능
+app.use('/loadLikes', (req, res) => {
+  let value = req.body.value;  
+  db.loadLikes(value, (rows) => {
+    res.json(rows); 
+  });
+});
+
+
+app.use('/Likes', (req, res) => {
+  let value = req.body.value;  
+  let name = req.body.user;
+  let title = req.body.title;
+
+  let userId = req.session.user.name;
+
+  db.checkLikes(value, userId, (exists) => {
+    if(!exists){
+      db.Likes(value, userId);
+      db.updateLikes(value, title, name);
+      res.send("좋아요!"); 
+    }else{
+      db.cancelLikes(value, userId);
+      db.deleteLikes(value, title, name);
+      res.send("좋아요 취소...");
+    }
+  });
+
+});
+
+app.use('/checkLikes', (req, res) => {
+  let value = req.body.value;
+
+  if (req.session.user) {
+    let userId = req.session.user.name;
+    db.checkLikes(value, userId, (exists) => {
+      res.send(exists ? true : false);
+    });
+  } else {
+    res.send(false);
+  }
+});
+
+app.use('/loadDislikes', (req, res) => {
+  let value = req.body.value;  
+  db.loadDislikes(value, (rows) => {
+    res.json(rows); 
+  });
+});
+
+app.use('/Dislikes', (req, res) => {
+  let value = req.body.value;  
+  let name = req.body.user;
+  let title = req.body.title;
+
+  let userId = req.session.user.name;
+
+  db.checkDislikes(value, userId, (exists) => {
+    if(!exists){
+      db.Dislikes(value, userId);
+      db.updateDislikes(value, title, name);
+      res.send("싫어요..."); 
+    }else{
+      db.cancelDislikes(value, userId);
+      db.deleteDislikes(value, title, name);
+      res.send("싫어요 취소!");
+    }
+  });
+});
+
+app.use('/checkDislikes', (req, res) => {
+  let value = req.body.value;
+
+  if (req.session.user) {
+    let userId = req.session.user.name;
+    db.checkDislikes(value, userId, (exists) => {
+      res.send(exists ? true : false);
+    });
+  } else {
+    res.send(false);
+  }
+});
+
---
+++ db.js
@@ -0,0 +1,263 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'1234',
+    port:3306,
+    database:'nodejs'
+});
+
+function getAllMemos(callback){
+    connection.query('SELECT * FROM board ORDER BY date DESC;', (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function getRankMemos(callback){
+    connection.query('SELECT * FROM board ORDER BY watch DESC, \`like\` DESC LIMIT 0, 5;', (err, data, fields) => {
+        if(err) throw err;
+        callback(data);
+    });
+}
+
+// function getSearchMemosPagenation(no, page_size, searchKeyword, callback){
+
+//     // 검색 키워드가 존재하는 경우, 제목에 검색 키워드가 포함된 레코드를 검색합니다.
+//     connection.query(`SELECT * FROM board WHERE title LIKE ${connection.escape('%' + searchKeyword + '%')} ORDER BY date DESC LIMIT ${no}, ${page_size}`, (err, rows, fields) => {
+//         if (err) throw err;
+//         callback(rows);
+//     });
+      
+// }
+
+function getMemosPagenation(no, page_size, searchKeyword, callback) {
+  if (!searchKeyword) {
+    connection.query(`SELECT * FROM board ORDER BY date DESC LIMIT ${no}, ${page_size}`, (err, rows, fields) => {
+      if (err) throw err;
+      callback(rows);
+    });
+  } else {
+    // 검색 키워드가 존재하는 경우, 제목에 검색 키워드가 포함된 레코드를 검색합니다.
+    connection.query(`SELECT * FROM board WHERE title LIKE ${connection.escape('%' + searchKeyword + '%')} ORDER BY date DESC LIMIT ${no}, ${page_size}`, (err, rows, fields) => {
+      if (err) throw err;
+      callback(rows);
+    });
+  }
+}
+
+function insertMemo(content, title, name){
+    connection.query(`INSERT INTO board (content, date, name, title) VALUES ('${content}',NOW(),'${name}', '${title}')`);
+}
+
+function getMemo(title, value, callback){
+    connection.query(`SELECT num, title, name, date, content, watch FROM board WHERE title = '${title}' && num = '${value}';`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function watchCount(title, value){
+    connection.query(`UPDATE board SET watch = watch + 1 WHERE title = '${title}' && num = '${value}'`);
+}
+
+function getUser(id, nickname, callback){
+    connection.query(`SELECT * FROM user WHERE id = '${id}';`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function insertUser(id, password, nickname){
+    connection.query(`INSERT INTO user(id, password, nickname) VALUES ('${id}','${password}', '${nickname}');`);
+}
+
+function checkUser(id, callback){
+    connection.query(`SELECT password, nickname FROM user WHERE id = '${id}';`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function getInfo(title, value, name, callback){
+    connection.query(`SELECT * FROM board WHERE title = '${title}' && num = '${value}' && name = '${name}';`, (err, rows, fields) => {
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function memoUpdate(title, value, name, content){
+    connection.query(`UPDATE board SET content = '${content}' WHERE title = '${title}' 
+                            AND num = '${value}' AND name = '${name}';`);
+}
+
+function memoDelete(title, name, value){
+    connection.query(`DELETE FROM board WHERE title = '${title}' && num = '${value}' && name = '${name}';`)
+}
+
+function commendInsert(value, content, user, commendId){
+    connection.query(`INSERT INTO commend(memo_id, commend_name, commend_date, commend_content, commend_id) 
+    VALUES ('${value}', '${user}', NOW(), '${content}', '${commendId}');`);
+}
+
+function getCommend(value, offset, limit, callback) {
+    connection.query(
+      `SELECT * FROM commend WHERE memo_id = '${value}' ORDER BY commend_date DESC LIMIT ${offset}, ${limit};`,
+      (err, rows, fields) => {
+        if (err) throw err;
+        callback(rows);
+      }
+    );
+  }
+
+  function getCommendCount(value, callback) {
+    connection.query(`SELECT COUNT(*) AS count FROM commend WHERE memo_id = '${value}';`, (err, rows, fields) => {
+      if (err) throw err;
+      callback(rows); // 댓글 전체 개수를 콜백으로 전달
+    });
+  }
+
+
+function getRecommend(commendId, offset, limit, callback) {
+    connection.query(
+      `SELECT * FROM recommend WHERE commend_id = '${commendId}' ORDER BY recommend_date DESC LIMIT ${offset}, ${limit};`,
+      (err, rows, fields) => {
+        if (err) throw err;
+        callback(rows);
+      }
+    );
+}
+function insertRecommend(commend_id, recommend_name, recommend_content, recommend_id) {
+    // `recommend_date`는 `NOW()`로 자동 설정되므로, `recommend_date`를 따로 쿼리에 넣지 않아도 됩니다.
+    connection.query(
+        `INSERT INTO recommend (commend_id, recommend_name, recommend_date, recommend_content, recommend_id) 
+        VALUES ('${commend_id}', '${recommend_name}', NOW(), '${recommend_content}', '${recommend_id}');`,
+        function (err, results) {
+            if (err) {
+                console.error("Error inserting recommend:", err);
+            } else {
+                console.log("Successfully inserted recommend:", results);
+            }
+        }
+    );
+}
+
+
+// 좋아요
+function loadLikes(value,callback){
+    connection.query(`SELECT COUNT(*) as count FROM likes 
+                        WHERE post_id='${value}';`, 
+        (err, rows, fields) => {
+            if(err) throw err;
+            callback(rows);
+    });
+}
+
+function Likes(value, userId){
+    connection.query(`INSERT INTO likes 
+            VALUES ('${value}','${userId}');`
+    );
+}
+
+function cancelLikes(value, userId){
+    connection.query(`DELETE FROM likes 
+            WHERE post_id='${value}' && user_id='${userId}';`
+    );
+}
+
+function checkLikes(value, userId, callback) {
+    connection.query(
+      `SELECT COUNT(*) AS count FROM likes 
+       WHERE post_id='${value}' AND user_id='${userId}';`,
+      (err, rows, fields) => {
+        if (err) throw err;
+        callback(rows[0].count > 0);
+      }
+    );
+}
+
+function updateLikes(value, title, name){
+    connection.query(`UPDATE board SET \`like\` = \`like\` + 1 WHERE 
+                 num = '${value}' && title = '${title}' && name = '${name}';`);
+}
+
+function deleteLikes(value, title, name){
+    connection.query(`UPDATE board SET \`like\` = \`like\` - 1 WHERE 
+                 num = '${value}' && title = '${title}' && name = '${name}';`);
+}
+  
+// 싫어요
+function loadDislikes(value, callback){
+    connection.query(`SELECT COUNT(*) as count FROM dislikes 
+                        WHERE post_id='${value}';`, 
+        (err, rows, fields) => {
+            if(err) throw err;
+            callback(rows);
+    });
+}
+
+function Dislikes(value, userId){
+    connection.query(`INSERT INTO dislikes 
+            VALUES ('${value}','${userId}');`
+    );
+}
+
+function cancelDislikes(value, userId){
+    connection.query(`DELETE FROM dislikes 
+            WHERE post_id='${value}' && user_id='${userId}';`
+    );
+}
+
+function checkDislikes(value, userId, callback) {
+    connection.query(
+      `SELECT COUNT(*) AS count FROM dislikes 
+       WHERE post_id='${value}' AND user_id='${userId}';`,
+      (err, rows, fields) => {
+        if (err) throw err;
+        callback(rows[0].count > 0);
+      }
+    );
+}
+
+function updateDislikes(value, title, name){
+    connection.query(`UPDATE board SET \`dislike\` = \`dislike\` + 1 WHERE 
+                 num = '${value}' && title = '${title}' && name = '${name}';`);
+}
+
+function deleteDislikes(value, title, name){
+    connection.query(`UPDATE board SET \`dislike\` = \`dislike\` - 1 WHERE 
+                 num = '${value}' && title = '${title}' && name = '${name}';`);
+}
+
+module.exports = {
+    getAllMemos,
+    insertMemo,
+    getMemo,
+    watchCount,
+    getUser,
+    insertUser,
+    checkUser,
+    getInfo,
+    memoUpdate,
+    memoDelete,
+    getMemosPagenation,
+    commendInsert,
+    getCommend,
+    getCommendCount,
+    getRecommend,
+    insertRecommend,
+    loadLikes,
+    Likes,
+    cancelLikes,
+    checkLikes,
+    updateLikes,
+    deleteLikes,
+    loadDislikes,
+    Dislikes,
+    cancelDislikes,
+    checkDislikes,
+    updateDislikes,
+    deleteDislikes,
+    getRankMemos
+}
